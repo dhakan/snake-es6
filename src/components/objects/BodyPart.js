322db933d68ef1021c2970b29bd817bc747848aa
Gustaf Elbander <gelbander@gmail.com>
2016-11-30 21:54:27 +0100

    Testing pixel bug.

--- src/components/objects/BodyPart.js
+++ src/components/objects/BodyPart.js
@@ -3,12 +3,12 @@ import Phaser from 'phaser';
 class BodyPart extends Phaser.Sprite {
 
     constructor(game, x, y) {
-        super(game, x, y, 'square');
+        super(game, x, y, 'fruit');
 
-        this.game.physics.enable(this);
-        this.body.collideWorldBounds = true;
         this.width = x;
         this.height = y;
+        this.game.physics.enable(this);
+        this.body.collideWorldBounds = true;
     }
 }
 
