85b111a7f2c2ff392e79c8d8515c18c7211e83ed
Gustaf Elbander <gelbander@gmail.com>
2017-01-04 21:48:12 +0100

    Test socket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import Game from 'src/components/objects/Game';
+import io from 'socket.io-client'
 
-new Game();
+const url = 'http://localhost:3000';
+const socket = io(url);
+
+const messages = {
+  YOU_CONNECTED: 'you-connected',
+  GAME_STARTED: 'game-started',
+  PLAYERS: 'players',
+}
+
+
+
+socket.on(messages.YOU_CONNECTED, (payload) => {
+  console.log('YOU_CONNECTED');
+});
+
+socket.on(messages.GAME_STARTED, (payload) => {
+  console.log('GAME STARTED!');
+});
+
+socket.on(messages.PLAYERS, (payload) => {
+  const players = new Map(payload);
+  console.log(players);
+});
